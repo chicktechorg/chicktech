e00d767602e283d6799d83caabfa2d6b005904f7
epicodus <student@epicodus.com>
2013-10-14 10:20:43 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+gem 'rails'
+gem 'pg'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder'
+gem 'devise'
+gem 'cancan'
+gem 'bootstrap-sass', git: 'https://github.com/thomas-mcdonald/bootstrap-sass'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'pry'
+end
+
+group :test do
+  gem 'better_errors'
+  gem 'quiet_assets'
+  gem 'shoulda-matchers'
+  gem 'capybara'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chicktech::Application.config.secret_key_base = '05600c88e602b235ece9e3d2bdd82298d3e2f868b2a1f42b6462e42478802c44b9dbacf6f2860683dfdcf39e0cbccfe867890d93133737484f3ce3f4c37dfe30'
