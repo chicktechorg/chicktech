d5ce0541896fbac0b83dafe645d5682694090298
Dustin Brown & Ana Tighe <student@epicodus.com>
2013-10-15 10:11:24 -0700

    add integration testing to sign in and sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  ROLES = %w[volunteer admin super_admin]
+
   validates_presence_of :first_name
   validates_presence_of :last_name      
   validates_presence_of :phone
+  validates_presence_of :role
 end
