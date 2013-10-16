fb27da2d2c966ffdad2f0899bc8e8b81b3d93ada
Dustin Brown & Ana Tighe <student@epicodus.com>
2013-10-15 17:29:25 -0700

    clean up users controller and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  # def index
-  #   @users = User.all
-  # end
+  def index
+    @users = User.all
+  end
 
   # def new
   #   @user = User.new
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   #   end
   # end
 
-  # def show
-  #  @user = User.find(params[:id])
-  # end
+  def show
+   @user = User.find(params[:id])
+  end
 
 private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Chicktech::Application.routes.draw do
   resources :events
   devise_for :users
-  resources :users 
+  resources :users, :only => [:show, :index]
   root :to => "static_pages#index"
 end
