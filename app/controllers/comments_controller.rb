e6b6b45b445187826e5f879c735bf6ea716c5f6e
Ana <anadances@gmail.com>
2013-10-24 15:59:49 -0700

    add comment abilities

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,31 @@ class CommentsController < ApplicationController::Base
     end
   end
 
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      flash[:notice] = "Your comment has been updated."
+      redirect_to @comment
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    flash[:notice] = "Your comment has been deleted."
+    redirect_to @comment.commentable
+  end
+
 private
   def comment_params
     params.require(:comment).permit(:content, :commentable_id, :commentable_type)
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,13 @@ class Ability
   def initialize(user)
     if user
       if user.role? :volunteer
-        can :read, [Event, Job, User, City]
-        can :update, Job
+        can :read, [Event, Job, User, City, Comment]
+        can :update, Job 
         can :manage, Task, :job => { :user_id => user.id }
+        can :manage, Comment, :user_id => user.id
       end
       if user.role? :admin
-        can :manage, [Event, Job, Task, City]
+        can :manage, [Event, Job, Task, City, Comment]
       end
       if user.role? :superadmin
         can :manage, :all
