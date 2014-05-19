3010123171f89ceecb9a050deea9adc91d8eff89
Dustin Brown & Corey Norris <coreypnorris@gmail.com>
2014-05-19 13:41:15 -0700

    add links for switching between months on the calendar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,7 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
     @events_by_date = @events.group_by(&:start_date)
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
     if params[:city]
       @events = Event.where(:city_id => params[:city][:city_id])
       respond_to do |format|
