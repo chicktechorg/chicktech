b8f6ad3fc8f1d3607430c3a5a1129e79190e2db7
Corey Norris <coreypnorris@gmail.com>
2014-06-02 14:49:14 -0700

    refactor broken tests

--- app/controllers/leadership_roles_controller.rb
+++ app/controllers/leadership_roles_controller.rb
@@ -6,11 +6,19 @@ class LeadershipRolesController < ApplicationController
     if params[:leadership_role][:signing_up]
       @leadership_role.update(user_id: current_user.id)
       flash[:notice] = "Congratulations! You have taken the lead of #{@leadership_role.leadable.name}."
-      redirect_to event_path(Event.find(@leadership_role.leadable[:event_id]))
+      if @leadership_role.leadable.instance_of? Event
+        redirect_to event_path(@leadership_role.leadable)
+      elsif @leadership_role.leadable.instance_of? Team
+        redirect_to event_path(Event.find(@leadership_role.leadable[:event_id]))
+      end
     elsif params[:leadership_role][:resigning]
       @leadership_role.update(user_id: nil)
       flash[:notice] = "The leader has resigned from #{@leadership_role.leadable.name}."
-      redirect_to event_path(Event.find(@leadership_role.leadable[:event_id]))
+      if @leadership_role.leadable.instance_of? Event
+        redirect_to event_path(@leadership_role.leadable)
+      elsif @leadership_role.leadable.instance_of? Team
+        redirect_to event_path(Event.find(@leadership_role.leadable[:event_id]))
+      end
     else
       @leadership_role.update(leadership_role_params)
       flash[:notice] = "#{@leadership_role.name} got updated."
