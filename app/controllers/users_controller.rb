88d7b58f9017be4f043f3fdaee12f28bfa18dd6f
Raouf Nabi & Corey Norris <student@epicodus.com>
2014-06-02 17:34:09 -0700

    remove extra space

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,6 @@ class UsersController < ApplicationController
 private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :phone, :password, :password_confirmation, :role, :city_id, :gender, :birthday)
+    params.require(:user).permit(:first_name, :last_name, :email, :phone, :password, :password_confirmation, :role, :city_id, :gender, :birthday, :photo, :photo_file_name, :photo_content_type, :photo_file_size, :photo_updated_at)
   end
 end
