0b331b0d0301bcfea527acab1278130a176cc22a
Corey Norris <coreypnorris@gmail.com>
2014-06-10 12:06:25 -0700

    refactor url in email methods

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,20 +4,20 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    @url = 'http://chicktech.org/login'
+    @url = 'http://chicktech.herokuapp.com/login'
     mail(to: @user.email, subject: 'Welcome to Chicktech')
   end
 
   def send_information(user)
     @user = user
-    @url = 'http://chicktech.org/login'
+    @url = 'http://chicktech.herokuapp.com/login'
     mail(to: @user.email, subject: 'ChickTech Updates')
   end
 
   def invite_volunteer(user, event)
     @user = user
     @event = event
-    @url = "http://chicktech.org/events/#{@event.id}"
+    @url = "http://chicktech.herokuapp.com/events/#{@event.id}"
 
     mail(to: @user.email, subject: "#{@event.name} has #{pluralize(@event.number_of_available_positions, 'more position')} to be filled")
   end
@@ -26,7 +26,7 @@ class UserMailer < ActionMailer::Base
     @admin = admin.pop
     @user = user
     @event = event
-    @url = "http://chicktech.org/events/#{@event.id}"
+    @url = "http://chicktech.herokuapp.com/events/#{@event.id}"
     mail(to: @admin.email, subject: "#{@user.first_name} #{@user.last_name} has requested the event leader position on #{@event.name}")
   end
 end
